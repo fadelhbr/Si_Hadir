b97b2f42ab35361c9051d47436702ee6488c98f4
E41232016_BINTANG ERLANGGA <158864333+bntangerl@users.noreply.github.com>
2024-10-22 08:52:00 +0700

    Update api_login.php

--- web/sihadir/api/api_login.php
+++ web/sihadir/api/api_login.php
@@ -1,10 +1,9 @@
 <?php
-// Koneksi ke database
-$conn = new mysqli("localhost", "root", "abc54321", "si_hadir");
+// koneksi ke database
+$conn = new mysqli("localhost", "root", "", "hadir");
 
-// Cek koneksi database
 if ($conn->connect_error) {
-    die(json_encode(["status" => "error", "message" => "Koneksi database gagal: " . $conn->connect_error]));
+    die("Connection failed: " . $conn->connect_error);
 }
 
 // Ambil data dari request Android
@@ -15,23 +14,25 @@ $password = $_POST['password'];
 error_log("Username: $username");
 error_log("Password: $password");
 
-// Cek apakah username dan password sudah diterima
-if (empty($username) || empty($password)) {
-    die(json_encode(["status" => "error", "message" => "Username dan password tidak boleh kosong"]));
-}
-
-// Query untuk memeriksa login dengan username dan password
-$sql = "SELECT * FROM Karyawan WHERE username='$username' AND password='$password'";
+// Query untuk memeriksa login
+$sql = "SELECT * FROM karyawan WHERE username='$username'";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    // Jika username dan password cocok
-    echo json_encode(["status" => "success", "message" => "Login berhasil"]);
+    $row = $result->fetch_assoc();
+    if ($password === $row['password']) {
+        echo json_encode([
+            "status" => "success",
+            "message" => "Login berhasil",
+            "nama" => $row['nama'], // Nama
+            "role" => $row['role'] // Role 
+        ]);
+    } else {
+        echo json_encode(["status" => "error", "message" => "Username atau password salah"]);
+    }
 } else {
-    // Jika username atau password salah
     echo json_encode(["status" => "error", "message" => "Username atau password salah"]);
 }
 
-// Tutup koneksi database
 $conn->close();
 ?>
